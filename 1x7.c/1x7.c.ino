4ab1548bfe19e96db5bf214e28bd061238ad2206
silhanr <silhanrom@gmail.com>
2020-04-19 15:17:32 +0200

    display 1x7 digital

---
+++ 1x7.c/1x7.c.ino
@@ -0,0 +1,184 @@
+/*
+ 
+    A
+   ---
+F |   | B
+  | G |
+   ---
+E |   | C
+  |   |
+   ---
+    D
+
+ 8 9 10 11 12
+ 6 7 8 9 10
+ | | | | | 
+ G F 8 A B
+
+ E D 3 C .
+ | | | | |
+ 1 2 3 4 5
+ 3 4 5 6 7 
+ */
+
+
+int pinA = 11;
+int pinB = 12;
+int pinC = 6;
+int pinD = 4;
+int pinE = 3;
+int pinF = 9;
+int pinG = 8;
+int pinDot = 7;
+int D1 = 5;
+int D2 = 10;
+
+
+void setup() {
+  // initialize the digital pins as outputs.
+  pinMode(pinA, OUTPUT);     
+  pinMode(pinB, OUTPUT);     
+  pinMode(pinC, OUTPUT);     
+  pinMode(pinD, OUTPUT);     
+  pinMode(pinE, OUTPUT);     
+  pinMode(pinF, OUTPUT);     
+  pinMode(pinG, OUTPUT);   
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);  
+
+}
+
+void displayNumber(int number){
+  switch(number){
+    case 0:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, LOW);
+      break;
+      
+    case 1:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, LOW);
+      break;
+      
+    case 2:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, LOW);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, HIGH);
+      break;
+      
+    case 3:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, HIGH);
+      break;
+      
+    case 4:
+      digitalWrite(D1, HIGH);
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      break;
+      
+    case 5:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, LOW);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      break;
+      
+    case 6:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, LOW);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      break;
+      
+    case 7:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, LOW);
+      break;
+
+    case 8:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      break;
+    case 9:
+      digitalWrite(D1, HIGH);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      break;
+    default:
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, LOW);
+      digitalWrite(pinC, LOW);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, HIGH);
+      break;
+  }
+  
+}
+
+
+void loop() {
+  
+  for (int i=0;i<10;i++){
+     displayNumber(i);
+     delay(1000);
+  }
+  // put your main code here, to run repeatedly:
+
+}
